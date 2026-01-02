24955f0e7faa040a4592084a14c4e542fb1a1296
pbai1ey <pbailey.losyork@gmail.com>
2026-01-02 13:40:46 -0800

    practice

--- main.py
+++ main.py
@@ -2,47 +2,89 @@ class Dog:
     def __init__(self):
         pass
 
+
 class Card:
     def __init__(self, suit):
         self.suit = suit
-        
-class Card:
-    def __init__(self,suit.rank):
-        self.suit = suit
-        self.rank = rank 
 
 
 class Card:
-    def __init__(self,suit,rank)
+    def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
-class Card:
-    def __init__(self,suit,rank):
-        self.suit = suit
-        self.rank.rank
 
-    
 class Card:
-    def __init__(self,suit,rank):
+    def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
+
 class Card:
-    def __init__(self,suit,rank):
+    def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
+
 class Card:
-    def __init__(self,suit,rank)
+    def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
+
 class Card:
-    def __init__(self,suit,rank)
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.rank
+        self.rank = rank
+
+
 class Card:
-    def __init__(self,suit,rank):
-            self.suit = suit
-            self.rank = rank
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+
+class Card:
+    def __init__(self):
+        pass
+
+
+class Card:
+    def __init__(self):
+        pass
+
+
+class Card:
+    def __init__(self):
+        pass
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+class 
