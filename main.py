46abcfb398c532efccc7b4de409a424efc917d6a
pbai1ey <pbailey.losyork@gmail.com>
2026-01-02 09:30:04 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+class Dog:
+    def __init__(self):
+        pass
+
+class Card:
+    def __init__(self, suit):
+        self.suit = suit
+        
+class Card:
+    def __init__(self,suit.rank):
+        self.suit = suit
+        self.rank = rank 
+
+
+class Card:
+    def __init__(self,suit,rank)
+        self.suit = suit
+        self.rank = rank
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank.rank
+
+    
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+
+class Card:
+    def __init__(self,suit,rank)
+        self.suit = suit
+        self.rank = rank
+
+class Card:
+    def __init__(self,suit,rank)
+        self.suit = suit
+        self.rank
+class Card:
+    def __init__(self,suit,rank):
+            self.suit = suit
+            self.rank = rank
